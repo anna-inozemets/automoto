88e304022d08d8ab7d266448f97d0c5150a19016
Anna Inozemets <inoznetta@gmail.com>
2023-06-06 15:39:55 +0300

    Tablet layout was implemented

--- index.js
+++ index.js
@@ -19,52 +19,27 @@ fileInput.addEventListener('change', function() {
 
 const tabButtons = document.querySelectorAll('.cases__button');
 const tabContents = document.querySelectorAll('.cases__cards');
-
-const carButton = document.querySelector('#carButton');
-const bikeButton = document.querySelector('#bikeButton');
 const allButton = document.querySelector('#allButton');
 
-const carContent = document.querySelector('#carContent');
-const bikeContent = document.querySelector('#bikeContent');
-
-carButton.addEventListener('click', function() {
-  tabButtons.forEach(tabButton => {
-    tabButton.classList.remove('active')
-  })
-  tabContents.forEach(tabContent => {
-    tabContent.classList.remove('active')
-  })
-
-  this.classList.add('active');
-  carContent.classList.add('active');
+tabButtons.forEach((tabButton, index) => {
+  tabButton.addEventListener('click', () => {
+    tabButtons.forEach(btn => btn.classList.remove('active'));
+    tabContents.forEach(content => content.classList.remove('active'));
+    
+    tabButton.classList.add('active');
+    tabContents[index].classList.add('active');
+  });
 });
 
-bikeButton.addEventListener('click', function() {
-  tabButtons.forEach(tabButton => {
-    tabButton.classList.remove('active')
-  })
-  tabContents.forEach(tabContent => {
-    tabContent.classList.remove('active')
-  })
-
-  this.classList.add('active');
-  bikeContent.classList.add('active');
+allButton.addEventListener('click', () => {
+  tabButtons.forEach(btn => btn.classList.remove('active'));
+  tabContents.forEach(content => content.classList.add('active'));
+  allButton.classList.add('active');
 });
 
-allButton.addEventListener('click', function() {
-  tabButtons.forEach(tabButton => {
-    tabButton.classList.remove('active')
-  })
-  tabContents.forEach(tabContent => {
-    tabContent.classList.add('active')
-  })
-
-  this.classList.add('active');
-});
-
-const openConnectModal = document.querySelector('.open-modal');
-const closeConnectModal = document.querySelector('.close-modal');
-const connectModal = document.querySelector('.modal')
+const openConnectModal = document.querySelector('.open-modal--aditional');
+const closeConnectModal = document.querySelector('.close-modal--additional');
+const connectModal = document.querySelector('.modal--additional')
 
 openConnectModal.addEventListener('click', function() {
   connectModal.classList.add('active');
@@ -73,3 +48,31 @@ openConnectModal.addEventListener('click', function() {
 closeConnectModal.addEventListener('click', function() {
   connectModal.classList.remove('active');
 })
+
+const openMainModal = document.querySelector('.open-modal-main');
+const closeMainModal = document.querySelector('.close-modal--main');
+const mainModal = document.querySelector('.modal--main');
+
+const buttonMoreContent = document.querySelector('.more-project-text');
+const moreContent = document.querySelector('.project__content-text');
+
+openMainModal.addEventListener('click', function() {
+  mainModal.classList.add('active');
+});
+
+closeMainModal.addEventListener('click', function() {
+  mainModal.classList.remove('active');
+})
+
+buttonMoreContent.addEventListener('click', function() {
+  const isHidden = moreContent.classList.contains('hidden');
+  moreContent.classList.toggle('hidden');
+
+  if (!isHidden) {
+    moreContent.style.maxHeight = `${270}px`;
+    buttonMoreContent.innerHTML = 'Більше';
+  } else {
+    moreContent.style.maxHeight = moreContent.scrollHeight + 'px';
+    buttonMoreContent.innerHTML = 'Менше';
+  }
+});
